3c8bc858cb7e89553ed1c3a0d588029cfaf2ac6a
Nabensa <39654321+Leonardox4@users.noreply.github.com>
2025-01-28 22:55:52 +0530

    Update Encrypter.sh

--- Encrypter.sh
+++ Encrypter.sh
@@ -3,23 +3,29 @@ echo "This is a simple Ecnryption/Decryption tool"
 echo "What is that you desire :"
 choice="Encrypt Decrypt"
 select option in $choice; do
-	if [ $REPLY = 1 ];then
-		echo "You have choosen encryption"
-		echo "Please enter the filename"
-		read file;
-		gpg -c $file
-		echo "Your file has been encrypted"
-		break
+        if [ $REPLY = 1 ];then
+                echo "You have choosen encryption"
+                echo "Please enter the filename"
+                read file;
+                gpg -c $file
+                rm -rf $file
+                echo "Your file has been encrypted"
+                break
 
-	elif [ $REPLY = 2 ]; then
-		echo "You have choosen decryption" 
-        	echo "Please enter the filename" 
-        	read file; 
-        	gpg -d $file
-        	echo "Your file has been decrypted"
-		break
-	else
-		echo "That's not a valid argument exiting......"
-		break
-	fi
+        elif [ $REPLY = 2 ]; then
+
+                echo "You have choosen decryption" 
+                echo "Please enter the filename :" 
+                read file;
+                output_file=${file%.gpg} 
+                gpg -d "$file" > "$output_file"
+                rm -rf $file
+
+                echo "Your file has been decrypted"
+                break
+        else
+                echo "That's not a valid argument exiting......"
+                break
+        fi
 done
+

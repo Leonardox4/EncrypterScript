f6b09aa21ddf92a703bba7e2deaf3720b8c853b5
Nabensa <39654321+Leonardox4@users.noreply.github.com>
2025-01-28 21:58:02 +0530

    Add files via upload

---
+++ Encrypter.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "This is a simple Ecnryption/Decryption tool"
+echo "What is that you desire :"
+choice="Encrypt Decrypt"
+select option in $choice; do
+	if [ $REPLY = 1 ];then
+		echo "You have choosen encryption"
+		echo "Please enter the filename"
+		read file;
+		gpg -c $file
+		echo "Your file has been encrypted"
+		break
+
+	elif[ $REPLY = 2 ];
+		echo "You have choosen decryption" 
+        	echo "Please enter the filename" 
+        	read file; 
+        	gpg -d $file
+        	echo "Your file has been decrypted"
+		break
+	else
+		echo "That's not a valid argument exiting......"
+		break
+	fi
+done
